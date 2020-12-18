cecfa92cba84078f02dd59ffb4a461cfe88dc556
Nicole Mastrostefano <nicole_mas@hotmail.it>
2020-12-18 13:44:20 +0100

    vue structure

--- js/script.js
+++ js/script.js
@@ -2,10 +2,30 @@ var app = new Vue (
   {
     el:"#root",
     data: {
+      movies:[],
 
+    },
+
+  methods:{
+    FilterMovie(){
+
+
+      axios
+      .get("https://api.themoviedb.org/3/search/movie",{
+        params: {
+          api_key: "adb08e6d46258c873174c3f5773f620a",
+          language:"it-IT",
+          query:
+        }
+      })
+      .then((result)=> {
+
+
+      })
     }
-
   }
+}
+}
+
+
 )
-// richiesta:https://api.themoviedb.org/3/movie/550?api_key=adb08e6d46258c873174c3f5773f620a
-// key: adb08e6d46258c873174c3f5773f620a
