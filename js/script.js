1b60d0c5b086f21b1ddd8bba8677a383d43f0063
Nicole Mastrostefano <nicole_mas@hotmail.it>
2020-12-18 12:06:47 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+var app = new Vue (
+  {
+    el:"#root",
+    data: {
+
+    }
+
+  }
+)
+// richiesta:https://api.themoviedb.org/3/movie/550?api_key=adb08e6d46258c873174c3f5773f620a
+// key: adb08e6d46258c873174c3f5773f620a
