64dc7b4010cdd737ddd2dff23ce0ff2930804dcb
Nicole Mastrostefano <nicole_mas@hotmail.it>
2020-12-22 23:42:49 +0100

    <select> genres

--- js/script.js
+++ js/script.js
@@ -7,15 +7,27 @@ var app = new Vue (
       imgPrefix:"https://image.tmdb.org/t/p/w220_and_h330_face",
       defaultPoster:"img/images.png",
       flags:["it","en","fr"],
+      genres: [],
+      genreUrl: "https://api.themoviedb.org/3/genre/movie/list?api_key=10a6546780c9082d52c54eb9c07f5d67",
+      genreSelected:"all",
+      loading: true,
     },
 
   methods:{
+    //  chiamata generi
+    filterGenres() {
+    axios
+      .get(this.genreUrl)
+      .then(response => {
+        this.genres = response.data.genres;
+      })
+    },
 
+    // ricerca per nome
     FilterMovie(){
       console.log(app.search);
       {if (this.search!= '') {
         this.movies = [];
-
 
         const params = {
           params: {
@@ -28,18 +40,18 @@ var app = new Vue (
         axios
         .get("https://api.themoviedb.org/3/search/movie", params)
         .then((result)=> {
-          this.movies=result.data.results
+          this.movies=this.movies.concat(result.data.results)
         })
-
 
         axios
         .get("https://api.themoviedb.org/3/search/tv", params)
         .then((result)=> {
           this.movies=this.movies.concat(result.data.results)
         })
-      }
-    }
-  },
+        .finally(() => this.loading = false)
+      }}
+
+    },
 
     // Funzione per arrotondare per eccesso all’unità successiva
     getAverage(star) {
@@ -49,12 +61,16 @@ var app = new Vue (
     // funzione per generare le bandiere
     getFlag(lang){
       return 'img/'+ lang +'.png';
-    }
+    },
+
+    getGenre(){}
+
+  },
+
+  created() {
+    this.filterGenres();
+
   },
 
 }
 )
-
-// JavaScript concat() Method : Array Object
-// it returns a copy of the same elements combined from the original arrays.
-// var joinarray=array1.concat(array2,array3);
