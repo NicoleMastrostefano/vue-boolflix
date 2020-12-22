71b092e4691fa11ca9a37a3825dfe6bc3412308d
Nicole Mastrostefano <nicole_mas@hotmail.it>
2020-12-22 15:24:23 +0100

    v-else

--- js/script.js
+++ js/script.js
@@ -6,42 +6,51 @@ var app = new Vue (
       search:"",
       imgPrefix:"https://image.tmdb.org/t/p/w220_and_h330_face",
       defaultPoster:"img/images.png",
-      flags:["it","en","fr"]
+      flags:["it","en","fr"],
     },
 
   methods:{
 
     FilterMovie(){
       console.log(app.search);
-      {
+      {if (this.search!= '') {
+        this.movies = [];
 
-      axios
 
-      .get("https://api.themoviedb.org/3/search/movie",{
-        params: {
-          api_key: "adb08e6d46258c873174c3f5773f620a",
-          language:"it-IT",
-          query:this.search
+        const params = {
+          params: {
+            api_key: "adb08e6d46258c873174c3f5773f620a",
+            language:"it-IT",
+            query:this.search
+          }
         }
-      })
 
-      .then((result)=> {
-        console.log(result.data.results);
-        this.movies=result.data.results
-      })
+        axios
+
+        .get("https://api.themoviedb.org/3/search/movie", params)
+        .then((result)=> {
+          this.movies=result.data.results
+        })
+
+      }
     }
   },
 
-  // Funzione per arrotondare per eccesso all’unità successiva
+    // Funzione per arrotondare per eccesso all’unità successiva
     getAverage(star) {
       return Math.ceil(star / 2);
     },
 
-  // funzione per generare le bandiere
+    // funzione per generare le bandiere
     getFlag(lang){
       return 'img/'+ lang +'.png';
     }
   },
 
-  }
+}
 )
+
+// provare a fare un concat per le serie tv
+// var hege = ["Cecilie", "Lone"];
+// var stale = ["Emil", "Tobias", "Linus"];
+// var children = hege.concat(stale);
